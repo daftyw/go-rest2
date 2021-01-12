f36acaeb6827cb47f579653154e230fd10921379
Rawin Ngamloet <rawin@MacBook-Pro.local>
2021-01-13 01:17:19 +0700

    add handler

--- main.go
+++ main.go
@@ -10,8 +10,17 @@ type MyHandler struct {
 
 // HandleFastHTTP : Handler for HTTP
 func (h *MyHandler) HandleFastHTTP(ctx *fasthttp.RequestCtx) {
+	switch string(ctx.Path()) {
+	case "/ping":
+		h.handlerPing(ctx)
+	default:
+		ctx.Error("Unsupported path", fasthttp.StatusNotFound)
+	}
+}
+
+func (h *MyHandler) handlerPing(ctx *fasthttp.RequestCtx) {
 	ctx.Response.Header.Set("content-type", "application/json")
-	ctx.Response.SetBody([]byte("{\"message\":\"Hello\""))
+	ctx.Response.SetBody([]byte("{\"message\":\"pong\"}"))
 }
 
 func main() {
